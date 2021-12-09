ef4345c662de3f0948c737b25c8d785092215380
Akmal Izuddin <izuddin0407@gmail.com>
2021-12-09 16:09:33 +0800

    made so that gameboard mark X if user clicked on the board and made a card that display whos turn but im thinking of making a factory function for that

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 //module for tic tac toe
 const ticTacToe = (function(){
-    const gameBoard = new Array(9);
+    const _gameBoard = new Array(9);
 
     const _board = document.querySelector('.board');
 
     const createBoard = function(){
-        for(i = 0; i < gameBoard.length; i++){
+        for(i = 0; i < _gameBoard.length; i++){
             let cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.textContent = 'x';
+            cell.onclick = function(){
+                if(cell.innerText === ''){
+                    cell.innerText = _playerOne.mark;
+                } else{
+                    return
+                }
+            };
 
             _board.appendChild(cell);
         }
     };
 
+    const _Player = function(name, mark){
+        this.name = name;
+        this.mark = mark;
+    };
+
+    const _playerOne = new _Player('Player One', 'X');
+    const _playerTwo = new _Player('Player Two', 'O');
+
+    const _cardContainer = document.querySelector('.card-container');
+
+    const createPlayerOneCard = function(){
+        let cardOne = document.createElement('div');
+        cardOne.classList.add('cardOne');
+        cardOne.textContent = `${_playerOne.name}: ${_playerOne.mark}`;
+
+        _cardContainer.appendChild(cardOne);
+    }
+
     return{
-        gameBoard,
         createBoard,
+        createPlayerOneCard,
     };
 })();
 
 ticTacToe.createBoard();
+ticTacToe.createPlayerOneCard();
