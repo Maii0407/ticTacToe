9158367238b699cfca9f6ae6fde3b704aef6a6a3
Akmal Izuddin <izuddin0407@gmail.com>
2021-12-11 07:45:08 +0800

    changed on how the gameboard is created to make it easier for me to detect who wins the fucking game. next im gonna make a function to detect who wins lol

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 //module for tic tac toe
 const ticTacToe = (function(){
-    const _gameBoard = new Array(9);
+    //this section is for creating the gameboard
+    //yes i know it looks ugly lol
+    const _gameBoard = [];
 
+    const Cell = function(no){
+        this.no = no
+    };
+
+    const Cell1 = new Cell(1);
+    const Cell2 = new Cell(2);
+    const Cell3 = new Cell(3);
+    const Cell4 = new Cell(4);
+    const Cell5 = new Cell(5);
+    const Cell6 = new Cell(6);
+    const Cell7 = new Cell(7);
+    const Cell8 = new Cell(8);
+    const Cell9 = new Cell(9);
+    _gameBoard.push(Cell1, Cell2, Cell3, Cell4, Cell5, Cell6, Cell7, Cell8, Cell9);
+
+    //this section creates the gameboard based on the length of _gameBoard
+    //this also inserts a function inside each cell of gameboard so players can input marks
     const _board = document.querySelector('.board');
 
-    //creates gameboard based on gameboard object length
     const createBoard = function(){
         for(i = 0; i < _gameBoard.length; i++){
             let cell = document.createElement('div');
@@ -31,7 +49,7 @@ const ticTacToe = (function(){
         }
     };
 
-    //the method to change turns also display current player turn
+    //this section is for determining current player turn
     let _nextTurn = 'x';
 
     const _changeTurn = function(){
@@ -49,7 +67,7 @@ const ticTacToe = (function(){
         }
     };
 
-    //factory function to create players
+    //this section is factory function to generate players
     const _Player = function(name, mark){
         this.name = name;
         this.mark = mark;
@@ -60,7 +78,7 @@ const ticTacToe = (function(){
 
     const _cardContainer = document.querySelector('.card-container');
 
-    //creates player cards for the display turn
+    //this section is factory function to generate player cards for the display
     const _PlayerCard = function(obj){
         let card = document.createElement('div');
         card.classList.add('card');
